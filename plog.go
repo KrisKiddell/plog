c1871c730c8513fd5aac7dd22ece4dfcb0a985c6
Kris Kiddell <kriskiddell@gmail.com>
2024-01-19 22:43:22 +0000

    testing

--- plog.go
+++ plog.go
@@ -5,4 +5,7 @@ import (
 	"os"
 )
 
-var Error = log.New(os.Stdout, "\uea87  ", log.LstdFlags|log.Lshortfile)
+var Error = log.New(os.Stdout, "\u001B[38;5;162m  Error in: \u001B[0m", log.Lshortfile)
+var Warn = log.New(os.Stdout, "\u001B[38;5;214m  Warning in: \u001B[0m", log.Lshortfile)
+var Info = log.New(os.Stdout, "\u001B[38;5;51m  Info: \u001B[0m", log.Lshortfile)
+var Success = log.New(os.Stdout, "\u001B[38;5;77m  Success: \u001B[0m", log.Lshortfile)
