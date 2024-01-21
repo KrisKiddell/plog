a950108110322db3b89b6478e1c8eaa6e67ad535
Kris Kiddell <kriskiddell@gmail.com>
2024-01-21 15:46:19 +0000

    Added custom log feature.

--- plog.go
+++ plog.go
@@ -1,6 +1,7 @@
 package plog
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -9,3 +10,7 @@ var Error = log.New(os.Stdout, "\u001B[38;5;162m  Error in: \u001B[0m", log.L
 var Warn = log.New(os.Stdout, "\u001B[38;5;214m  Warning in: \u001B[0m", log.Lshortfile)
 var Info = log.New(os.Stdout, "\u001B[38;5;51m  Info: \u001B[0m", log.Lshortfile)
 var Success = log.New(os.Stdout, "\u001B[38;5;77m  Success: \u001B[0m", log.Lshortfile)
+
+func CustomLog(color int, icon string, prefix string) *log.Logger {
+	return log.New(os.Stdout, fmt.Sprintf("\u001B[38;5;%dm %s %s: \u001B[0m", color, icon, prefix), log.Lshortfile)
+}
