d300bca3da92f3e288a1373cd0e722539d771ebf
Kris Kiddell <kriskiddell@gmail.com>
2024-06-11 19:53:32 +0100

    dunno

--- plog.go
+++ plog.go
@@ -11,6 +11,7 @@ var Warn = log.New(os.Stdout, "\u001B[38;5;214m  Warning in: \u001B[0m", log.
 var Info = log.New(os.Stdout, "\u001B[38;5;51m  Info: \u001B[0m", log.Lshortfile)
 var Success = log.New(os.Stdout, "\u001B[38;5;77m  Success: \u001B[0m", log.Lshortfile)
 
+// Go crazy with it!
 func CustomLog(color int, icon string, prefix string) *log.Logger {
 	return log.New(os.Stdout, fmt.Sprintf("\u001B[38;5;%dm %s %s: \u001B[0m", color, icon, prefix), log.Lshortfile)
 }
